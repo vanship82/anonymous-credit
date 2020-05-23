973f7ff7b8a58a0d4e3194fcb2fbb5eeeb8865b1
vanship82 <kwen07@gmail.com>
2020-05-23 11:03:32 +0000

    more options to geth docker

--- start-geth-docker.sh
+++ start-geth-docker.sh
@@ -6,6 +6,7 @@ docker run -it -p 30303:30303 -p 8545:8545 \
     -v $DIR/geth-data:/root/.ethereum \
     ethereum/client-go:stable \
     --ropsten \
+    --allow-insecure-unlock \
     --ipcdisable \
-    --rpc --rpcaddr "0.0.0.0" \
+    --rpc --rpcaddr "0.0.0.0" --rpcapi "eth,web3,personal,net" \
     $@
