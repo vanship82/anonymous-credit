c959f2c744fcb5a75ac6129713d639aeb6186fbf
vanship82 <kwen07@gmail.com>
2020-05-24 14:23:52 +0000

    new ac contract address

--- src/web3c/index.js
+++ src/web3c/index.js
@@ -18,7 +18,7 @@ if (window.ethereum) {
         })
     })
 }
-const acContract = new web3.eth.Contract(require('./anonymous_credit_abi.json'), '0x1d9c667325e12e2fd1329ceb51752dac3d5e5ca2');
+const acContract = new web3.eth.Contract(require('./anonymous_credit_abi.json'), '0x98565C9D33e609412dD275d68f6Bd02A4664BFDd');
 const amContract = new web3.eth.Contract(require('./address_mining_abi.json'), '0xaA737aBA880090F3fb5161BEA76f7c22864a2CCe');
 const cbtcContract = new web3.eth.Contract(require('./ierc20_abi.json'), '0xa3619656EDDD61cf7a06b7a5eA37Da72D45fE224');
 export { web3, acContract, amContract, cbtcContract };
